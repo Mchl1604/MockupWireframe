56f26be74e06e762624b774a62b9c56fbb7644bb
Michael A. Capanayan <154234880+Mchl1604@users.noreply.github.com>
2026-04-10 21:50:01 +0800

    Update header.php with new navbar content

---
+++ partials/header.php
@@ -0,0 +1,17 @@
+<?php
+// Shared header + navbar
+?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/MockupWireframe/index.php">Cool Construct Hub</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item"><a class="nav-link" href="/MockupWireframe/index.php">Landing</a></li>
+      <li class="nav-item"><a class="nav-link" href="/MockupWireframe/pages/login.php">Login</a></li>
+      <li class="nav-item"><a class="nav-link" href="/MockupWireframe/pages/register.php">Register</a></li>
+    </ul>
+    <span class="navbar-text text-light small">Front-end only mockup</span>
+  </div>
+</nav>
